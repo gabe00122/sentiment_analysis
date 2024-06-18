433c29cadefc404e8af865376a4edcabe39f89ec
Gabriel keith <gabe.keith@yahoo.com>
2024-06-17 22:00:03 -0400

    wraping up checkpointer

--- sentiment_analysis/common/checkpointer.py
+++ sentiment_analysis/common/checkpointer.py
@@ -5,7 +5,9 @@ import jax
 
 
 class Checkpointer:
-    def __init__(self, directory: Path):
+    def __init__(self, directory: str | Path):
+        directory = Path(directory)
+        directory = directory.absolute()
         self.mngr = ocp.CheckpointManager(directory)
 
     def save(self, step: int, model: nnx.Module):
@@ -13,10 +15,13 @@ class Checkpointer:
         self.mngr.save(step, args=ocp.args.StandardSave(state))
 
     def restore(self, step: int, model: nnx.Module) -> nnx.Module:
-        _, state = nnx.split(model)
+        graphdef, state = nnx.split(model)
         abstract_state = jax.tree_util.tree_map(ocp.utils.to_shape_dtype_struct, state)
 
-        return self.mngr.restore(step, args=ocp.args.StandardRestore(abstract_state))
+        restored_state = self.mngr.restore(
+            step, args=ocp.args.StandardRestore(abstract_state)
+        )
+        return nnx.merge(graphdef, restored_state)
 
     def restore_latest(self, model: nnx.Module) -> nnx.Module:
         return self.restore(self.mngr.latest_step(), model)
