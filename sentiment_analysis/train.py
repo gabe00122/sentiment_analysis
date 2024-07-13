4cfcdf4b3dfda3b8b2abd6c1dadaee445a94504c
Gabriel Keith <gfk1995@gmail.com>
2024-07-13 12:48:00 -0400

    jit wrapper

--- sentiment_analysis/train.py
+++ sentiment_analysis/train.py
@@ -1,6 +1,8 @@
 import os
+import time
 from functools import partial
 from pathlib import Path
+from typing import NamedTuple
 
 import jax
 import optax
@@ -81,50 +83,91 @@ def train(experiment: Experiment):
 
     writer = TensorboardWriter(Path("./tensorboard"))
 
-
     for epoch in range(settings.epochs):
         shuffle_key, consume_rng = random.split(shuffle_key)
         indices = random.permutation(consume_rng, indices)
 
-        for step in range(steps):
-            metrics = train_step(optimizer, indices, tokens, labels, settings.batch_size, step, rngs)
+        for step in range(20):
+            start_time = time.time()
+            metrics, optimizer, rngs = jit_train_step(optimizer, rngs, Static(settings.batch_size, False), Dynamic(step, indices, tokens, labels))
             writer.write(metrics)
 
             loss = metrics["loss"].mean().item()
             percent_correct = metrics["percent_correct"].mean().item() * 100
 
-            print(f"epoch = {epoch}/{settings.epochs}, step = {step}/{steps}, loss = {loss}, correct = {percent_correct:.0f}%")
+            end_time = time.time()
+            delta_time = end_time - start_time
+            samples_per_second = settings.batch_size / delta_time
+
+            print(f"epoch = {epoch}/{settings.epochs}, step = {step}/{steps}, loss = {loss}, correct = {percent_correct:.0f}%, perf = {samples_per_second:.2f}")
+
 
         checkpoints.save(optimizer.model, epoch)
 
     writer.close()
     checkpoints.close()
 
+class Static(NamedTuple):
+    batch_size: int
+    metrics_only: bool
 
-@partial(nnx.jit, donate_argnums=(0, 6), static_argnums=4)
-def train_step(optimizer: nnx.Optimizer, indices: jax.Array, tokens: jax.Array, labels: jax.Array, batch_size: int, step: int, rngs: nnx.Rngs) -> Metrics:
+
+class Dynamic(NamedTuple):
+    step: jax.typing.ArrayLike
+    indices: jax.Array
+    tokens: jax.Array
+    labels: jax.Array
+
+
+def loss_fn(model, tokens, labels, rngs):
+    logit_pred = model(tokens, deterministic=False, rngs=rngs)
+
+    loss = jnp.mean(optax.softmax_cross_entropy_with_integer_labels(logit_pred, labels))
+
+    predicted_labels = jnp.argmax(logit_pred, axis=-1)
+    percent_correct = jnp.mean(predicted_labels == labels, dtype=jnp.float32)
+    metrics = {"percent_correct": percent_correct, "loss": loss}
+
+    return loss, metrics
+
+def train_step(optimizer: nnx.Optimizer, rngs: nnx.Rngs, static: Static, dynamic: Dynamic) -> Metrics:
     model = optimizer.model
 
     indices = jax.lax.dynamic_slice(
-        indices, (batch_size * step,), (batch_size,)
+        dynamic.indices, (static.batch_size * dynamic.step,), (static.batch_size,)
     )
-    tokens = tokens[indices]
-    labels = labels[indices]
+    tokens = dynamic.tokens[indices]
+    labels = dynamic.labels[indices]
 
-    def loss_fn(model):
-        logit_pred = model(tokens, deterministic=False, rngs=rngs)
-
-        loss = jnp.mean(optax.softmax_cross_entropy_with_integer_labels(logit_pred, labels))
-
-        predicted_labels = jnp.argmax(logit_pred, axis=-1)
-        percent_correct = jnp.mean(
-            predicted_labels == labels, dtype=jnp.float32
-        )
-        metrics = {"percent_correct": percent_correct, "loss": loss}
-
-        return loss, metrics
-
-    grads, metrics = nnx.grad(loss_fn, has_aux=True, wrt=nnx.Param)(model)
-    optimizer.update(grads)
+    if static.metrics_only:
+        _, metrics = loss_fn(model, tokens, labels, rngs)
+    else:
+        grads, metrics = nnx.grad(loss_fn, has_aux=True, wrt=nnx.Param)(model, tokens, labels, rngs)
+        optimizer.update(grads)
 
     return metrics
+
+
+@partial(jax.jit, static_argnames=('optimizer_graph', 'rngs_graph', 'static'), donate_argnames=('optimizer_state', 'rngs_state'))
+def wrapper(optimizer_graph, optimizer_state, rngs_graph, rngs_state, static, dynamic):
+    optimizer = nnx.merge(optimizer_graph, optimizer_state)
+    rngs = nnx.merge(rngs_graph, rngs_state)
+
+    metrics = train_step(optimizer, rngs, static, dynamic)
+
+    optimizer_state = nnx.state(optimizer)
+    rngs_state = nnx.state(rngs)
+
+    return metrics, optimizer_state, rngs_state
+
+
+def jit_train_step(optimizer: nnx.Optimizer, rngs: nnx.Rngs, static: Static, dynamic: Dynamic) -> (Metrics, nnx.Optimizer, nnx.Rngs):
+    optimizer_graph, optimizer_state = nnx.split(optimizer)
+    rngs_graph, rngs_state = nnx.split(rngs)
+
+    metrics, optimizer_state, rngs_state = wrapper(optimizer_graph, optimizer_state, rngs_graph, rngs_state, static, dynamic)
+
+    optimizer = nnx.merge(optimizer_graph, optimizer_state)
+    rngs = nnx.merge(rngs_graph, rngs_state)
+
+    return metrics, optimizer, rngs
